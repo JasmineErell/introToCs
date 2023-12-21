06fa91e9ba2a3cee979e81513b4c3ee93336afe9
JasmineErell <jasmine.erell@gmail.com>
2023-12-21 19:30:42 +0200

    fix 2

--- Add2Num.java
+++ Add2Num.java
@@ -3,6 +3,6 @@ class Add2Num {
     	int a = Integer.parseInt(args[0]);
 	    int b = Integer.parseInt(args[1]);
 	    int c = a+b;
-        System.out.println(a + "+" + b + "=" + c);
+        System.out.println(a + " " + "+" + " " + b + " " + "=" + " " + c);
     }
 }
