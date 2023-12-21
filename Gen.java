62ca2cf50d0eb4a7aa1d5fbcd288af4daa64451e
JasmineErell <jasmine.erell@gmail.com>
2023-12-21 15:43:47 +0200

    home work 1

---
+++ Gen.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+class Gen {
+    public static void main(String[] args) {
+        Random rand = new Random();
+    	int a = Integer.parseInt(args[0]);
+	    int b = Integer.parseInt(args[1]);
+        int max = Math.max(a,b);
+        int min = Math.min(a,b);
+        int ran1 = rand.nextInt(max - min + 1) + min;
+        int ran2 = rand.nextInt(max - min + 1) + min;
+        int ran3 = rand.nextInt(max - min + 1) + min;
+        System.out.println(ran1);
+        System.out.println(ran2);
+        System.out.println(ran3);
+        int min1 = Math.min(ran1, ran2);
+        int miniest = Math.min(min1, ran3);
+        System.out.println("the smallest number is " + miniest);
+    }
+}
---
+++ Triangle.java
@@ -0,0 +1,9 @@
+class Triangle {
+    public static void main(String[] args) {
+    	int a = Integer.parseInt(args[0]);
+	    int b = Integer.parseInt(args[1]);
+	    int c = Integer.parseInt(args[2]);
+        boolean d = ((a+b>c) && (a+c>b) && (c+b>a));
+        System.out.println(a + "," + b + "," + c + ":" + d);
+    }
+}
